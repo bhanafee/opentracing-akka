3be3835cc480cc8c1cf2accc4b88652e78e7105f
Brian Hanafee <bhanafee@gmail.com>
2017-04-30 22:01:49 -0700

    TextMapCarrierSpec passes with mock that includes baggage

--- src/test/scala/io/opentracing/contrib/akka/TextMapCarrierSpec.scala
+++ src/test/scala/io/opentracing/contrib/akka/TextMapCarrierSpec.scala
@@ -1,8 +1,7 @@
 package io.opentracing.contrib.akka
 
 import io.opentracing.SpanContext
-import io.opentracing.mock.{MockSpan, MockTracer}
-import io.opentracing.mock.MockSpan.MockContext
+import io.opentracing.mock.MockSpan
 
 import scala.util.Try
 
@@ -12,7 +11,7 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
   def testSpan(): MockSpan = tracer.buildSpan("operation").start()
 
   it should "generate span context data" in {
-    val test: MockContext = testSpan().context()
+    val test: MockSpan.MockContext = testSpan().context()
     assert(test.spanId() > 0L)
     assert(test.traceId() > 0L)
 
@@ -24,14 +23,14 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
     result.get("traceid").isEmpty should be (false)
   }
 
-  ignore should "generate baggage items" in {
+  it should "generate baggage items" in {
     val test = testSpan().setBaggageItem("key1", "value1").setBaggageItem("key2", "value2").context()
-    assert(!test.baggageItems().iterator().hasNext)
+    assert(test.baggageItems().iterator().hasNext)
 
     val result: Map[String, String] = TextMapCarrier.inject(tracer)(test)
 
-    result.get("key1") should be (Some("key1"))
-    result.get("key2") should be (Some("key2"))
+    result.get("key1") should be (Some("value1"))
+    result.get("key2") should be (Some("value2"))
   }
 
   it should "extract payload data" in {
@@ -40,7 +39,7 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
     val result: Try[SpanContext] = TextMapCarrier.extract(tracer)(test)
 
     result.isSuccess should be (true)
-    val mock = result.get.asInstanceOf[MockContext]
+    val mock = result.get.asInstanceOf[MockSpan.MockContext]
     mock.spanId() should be (13L)
     mock.traceId() should be (17L)
   }
@@ -61,7 +60,7 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
     result.isFailure should be (true)
   }
 
-  ignore should "extract baggage items" in {
+  it should "extract baggage items" in {
     val test: Map[String, String] = Map(("spanid", "13"),("traceid", "17"),("key1", "value1"))
 
     val result: Try[SpanContext] = TextMapCarrier.extract(tracer)(test)
@@ -69,7 +68,9 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
     result.isSuccess should be (true)
     val resultIter = result.get.baggageItems().iterator()
     resultIter.hasNext should be (true)
-    resultIter.next() should be (("key1", "value1"))
+    val baggage = resultIter.next()
+    baggage.getKey should be ("key1")
+    baggage.getValue should be ("value1")
     resultIter.hasNext should be (false)
   }
 
