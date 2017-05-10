01c1fcc285c6a73bb4838838dbdbc7cf23b47505
Brian Hanafee <bhanafee@gmail.com>
2017-05-10 00:35:43 -0700

    Implement stacked tagging

--- src/main/scala/io/opentracing/contrib/akka/TracingReceive.scala
+++ src/main/scala/io/opentracing/contrib/akka/TracingReceive.scala
@@ -1,65 +1,94 @@
 package io.opentracing.contrib.akka
 
+import java.time.Instant
+import java.time.temporal.ChronoField
+
+import scala.util.{Failure, Success}
 import akka.actor.Actor.Receive
+import akka.actor.ActorRef
 import io.opentracing.Tracer.SpanBuilder
-import io.opentracing.{References, SpanContext}
+import io.opentracing.References
 
-/** Decorator to add a Span around a Receive. Akka messages are one-way, so references
-  * to the received context are `FOLLOWS_FROM` rather than `CHILD_OF` */
-class TracingReceive(state: Spanned, r: Receive) extends Receive {
-
-  /**
-    * Wraps a call to `r` in a `Span`, where the `Span` itself is maintained by `SpanState`
-    *
-    * @param contextualize typically adds references to other spans
-    * @param v1            the original parameter for `r`
-    */
-  private def wrap(contextualize: SpanBuilder => SpanBuilder, v1: Any): Unit = {
-    state.span = contextualize(state.tracer.buildSpan(state.operation)).start()
-    r(v1)
-    state.span.finish()
-  }
+/** Decorator to add an OpenTracing Span around a Actor's Receive. */
+class TracingReceive(r: Receive,
+                     state: Spanned,
+                     operation: TracingReceive.Operation,
+                     modifiers: TracingReceive.Modifier*)
+  extends Receive {
 
   /** Proxies `r.isDefinedAt` */
   override def isDefinedAt(x: Any): Boolean = r.isDefinedAt(x)
 
   /** Invokes `r.apply` in the context of a new `Span` held by `state`.  The span is built,
     * started, and finished automatically. */
-  override def apply(v1: Any): Unit = v1 match {
-    case c: Carrier[_]#Traceable =>
-      // Extract the SpanContext from the payload
-      val sc: Option[SpanContext] = (c.trace match {
-        case b: Array[Byte] => BinaryCarrier.extract(state.tracer, b)
-        case m: Map[String, String] => TextMapCarrier.extract(state.tracer, m)
-      }).toOption
-      // Create a builder that will add a reference to the context
-      val follows = (b: SpanBuilder) => sc match {
-        case Some(s) => b.addReference(References.FOLLOWS_FROM, s)
-        case None => b
-      }
-      wrap(sb => TracingReceive.tag(c)(follows(sb)), c)
-    case c => wrap(TracingReceive.tag(c), c)
+  override def apply(v1: Any): Unit = {
+    val z: SpanBuilder = state.tracer.buildSpan(operation(v1))
+    val op: (SpanBuilder, TracingReceive.Modifier) => SpanBuilder = (sb, m) => m(sb, v1)
+    val sb = modifiers.foldLeft(z)(op)
+    state.span = sb.withStartTimestamp(nowMicroseconds()).start()
+    r(v1)
+    state.span.finish()
+  }
+
+  def nowMicroseconds(): Long = {
+    val now = Instant.now()
+    val secs = now.getLong(ChronoField.INSTANT_SECONDS)
+    val micros = now.getLong(ChronoField.MICRO_OF_SECOND)
+    secs * 1000000 + micros
   }
 
 }
 
 object TracingReceive {
-  def apply(state: Spanned)(r: Receive): Receive = new TracingReceive(state, r)
+  /** Used to specify the span's operation name. */
+  type Operation = Any => String
 
+  /** Used to stack SpanBuilder operations */
+  type Modifier = (SpanBuilder, Any) => SpanBuilder
 
-  def tag(v1: Any): SpanBuilder => SpanBuilder = _.withTag("message", v1.getClass.getName)
+  def apply(state: Spanned, operation: Operation, modifiers: Modifier*)(r: Receive): Receive =
+    new TracingReceive(r, state, operation, modifiers: _*)
 
+  def apply(state: Spanned)(r: Receive): Receive =
+    new TracingReceive(r, state, messageClassIsOperation, follows(state), tagAkkaComponent)
 
-  /* TODO: figure out reference strategy.
-     - Always FOLLOW_FROM? Or if CHILD_OF useful for always sender()?
-     - What is correct SpanContext in a send back to sender()?
-     - How to recognize return messages?
-  */
+  def apply(state: Spanned, ref: ActorRef)(r: Receive) =
+    new TracingReceive(r, state, messageClassIsOperation, follows(state), tagAkkaComponent, tagActorUri(ref))
 
-  // TODO: parameterize operation name (set via default in SpanState or during builder or in Span)
+  def constantOperation(operation: String): Operation = _ => operation
 
-  // TODO: stackable functions to add tags
-  // _.withTag("message", v1.getClass.getName)
+  def messageClassIsOperation: Operation = _.getClass.getName
 
-  // TODO: parameterize tag for message type (warn about simpleName and canonicalName)
+  def actorNameIsOperation(ref: ActorRef): Operation = constantOperation(ref.path.name)
+
+  /** Akka messages are one-way, so by default references to the received context are
+    * `FOLLOWS_FROM` rather than`CHILD_OF` */
+  def follows(state: Spanned, reference: String = References.FOLLOWS_FROM): Modifier =
+    (b: SpanBuilder, m: Any) => m match {
+      case c: Carrier[_]#Traceable =>
+        // Extract the SpanContext from the payload
+        (c.trace match {
+          case b: Array[Byte] => BinaryCarrier.extract(state.tracer, b)
+          case m: Map[String, String] => TextMapCarrier.extract(state.tracer, m)
+        }) match {
+          case Success(s) => b.addReference(reference, s)
+          case Failure(_) => b
+        }
+      case _ => b
+    }
+
+  val tagAkkaComponent: Modifier =
+    (b: SpanBuilder, _) => b.withTag("component", "akka")
+
+  def tagActorUri(ref: ActorRef): Modifier =
+    (b: SpanBuilder, _) => b.withTag("akka.uri", ref.path.name)
+
+  def tag(name: String, extract: Any => String): Modifier =
+    (b: SpanBuilder, m: Any) => b.withTag(name, extract(m))
+
+//  def tag(name: String, extract: Any => Boolean): Modifier =
+//    (b: SpanBuilder, m: Any) => b.withTag(name, extract(m))
+//
+//  def tag(name: String, extract: Any => Number): Modifier =
+//    (b: SpanBuilder, m: Any) => b.withTag(name, extract(m))
 }
