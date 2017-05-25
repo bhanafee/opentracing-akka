353a934515c306b66a9965d08149695cfe0e1857
Brian Hanafee <bhanafee@gmail.com>
2017-05-25 11:07:34 -0700

    Error handling when 'apply' throws an exception

--- src/main/scala/io/opentracing/contrib/akka/TracingReceive.scala
+++ src/main/scala/io/opentracing/contrib/akka/TracingReceive.scala
@@ -3,11 +3,11 @@ package io.opentracing.contrib.akka
 import java.time.Instant
 import java.time.temporal.ChronoField
 
-import scala.util.{Failure, Success}
+import scala.util.{Failure, Success, Try}
 import akka.actor.Actor.Receive
 import akka.actor.ActorRef
 import io.opentracing.Tracer.SpanBuilder
-import io.opentracing.References
+import io.opentracing.{References, Span}
 
 /** Decorator to add an OpenTracing Span around an Actor's Receive. */
 class TracingReceive(r: Receive,
@@ -20,14 +20,30 @@ class TracingReceive(r: Receive,
   override def isDefinedAt(x: Any): Boolean = r.isDefinedAt(x)
 
   /** Invokes `r.apply` in the context of a new `Span` held by `state`.  The span is built,
-    * started, and finished automatically. */
+    * started, and finished by this wrapper using manual span propagation. Span propagation
+    * using an `ActiveSpan` is not necessary because the Akka programming model guarantees
+    * single threaded execution of `apply`. */
   override def apply(v1: Any): Unit = {
     val z: SpanBuilder = state.tracer.buildSpan(operation(v1))
     val op: (SpanBuilder, TracingReceive.Modifier) => SpanBuilder = (sb, m) => m(sb, v1)
     val sb = modifiers.foldLeft(z)(op)
     state.span = sb.startManual()
-    r(v1)
-    state.span.finish()
+    Try (r(v1)) match {
+      case Success(_) => state.span.finish()
+      case Failure(e) =>
+        error(state.span, e)
+        state.span.finish()
+        throw e
+    }
+  }
+
+  /** Tag and log an exception thrown by `apply` */
+  def error(span: Span, e: Throwable): Unit = {
+    span.setTag("error", true)
+    val fields: java.util.Map[String, Any] = new java.util.HashMap[String, Any]
+    fields.put("event", "error")
+    fields.put("error.object", e)
+    span.log(fields)
   }
 }
 
