2c07858fc0de0aa075c56590ba398074766f8cc5
Brian Hanafee <bhanafee@gmail.com>
2017-05-26 13:37:40 -0700

    Simplify carrier tests

--- src/test/scala/io/opentracing/contrib/akka/BinaryCarrierSpec.scala
+++ src/test/scala/io/opentracing/contrib/akka/BinaryCarrierSpec.scala
@@ -1,7 +1,6 @@
 package io.opentracing.contrib.akka
 
 import io.opentracing.SpanContext
-import io.opentracing.mock.MockSpan.MockContext
 import io.opentracing.mock.MockSpan
 
 import scala.util.Try
@@ -11,60 +10,22 @@ class BinaryCarrierSpec extends AbstractTracingSpec {
 
   def testSpan(): MockSpan = tracer.buildSpan("operation").start()
 
-  "A binary carrier" should "handle an empty payload" in {
-    val test = Array.emptyByteArray
+  "A binary carrier" should "carry span context data" in pending
 
-    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, test)
+  it should "handle baggage" in pending
+
+  "A binary carrier" should "handle an empty payload" in {
+    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, Array.emptyByteArray)
 
     result shouldBe 'isFailure
-  }
-
-  ignore should "generate span context data" in {
-    val test: MockContext = testSpan().context()
-
-    val result: Array[Byte] = BinaryCarrier.inject(tracer, test)
-
-    //assert(result.length > 0, "Empty array injected")
-    // TODO: How to check for spanid, traceid
-  }
-
-  ignore should "generate baggage items" in {
-    val test = testSpan().setBaggageItem("key1", "value1").setBaggageItem("key2", "value2")
-
-    val result: Array[Byte] = BinaryCarrier.inject(tracer, test.context())
-
-    // TODO: How to check?
-  }
-
-  ignore should "extract payload data" in {
-    val test = Array.emptyByteArray // TODO: encode spanid = 13, traceid = 17
-
-    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, test)
-
-    result shouldBe 'isSuccess
-    val mock = result.get.asInstanceOf[MockContext]
-    mock.spanId() should be (13L)
-    mock.traceId() should be (17L)
   }
 
   it should "handle a malformed payload" in {
-    val test = Array[Byte](0, 1)
+    val malformed = Array[Byte](0, 1)
 
-    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, test)
+    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, malformed)
 
     result shouldBe 'isFailure
-  }
-
-  ignore should "extract baggage items" in {
-    val test = Array.emptyByteArray // TODO: encode spanid = 13, traceid = 17, key1 = value1
-
-    val result: Try[SpanContext] = BinaryCarrier.extract(tracer, test)
-
-    result shouldBe 'isSuccess
-    val resultIter = result.get.baggageItems().iterator()
-    resultIter shouldBe 'hasNext
-    resultIter.next() should be (("key1", "value1"))
-    resultIter.hasNext should be (false)
   }
 
 }
--- src/test/scala/io/opentracing/contrib/akka/TextMapCarrierSpec.scala
+++ src/test/scala/io/opentracing/contrib/akka/TextMapCarrierSpec.scala
@@ -10,68 +10,48 @@ class TextMapCarrierSpec extends AbstractTracingSpec {
 
   def testSpan(): MockSpan = tracer.buildSpan("operation").start()
 
-  "A text map carrier"  should "handle an empty payload" in {
-    val test: Map[String, String] = Map.empty
+  "A text map carrier" should "carry span context data" in {
+    val original: MockSpan.MockContext = testSpan().context()
+    assume(original.spanId() > 0L, "Span id wasn't initialized")
+    assume(original.traceId() > 0L, "Trace id wasn't initialized")
 
-    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, test)
+    val carrier: Map[String, String] = TextMapCarrier.inject(tracer, original)
+    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, carrier)
 
-    result shouldBe 'isFailure
+    result shouldBe 'isSuccess
+    val ctx = result.get.asInstanceOf[MockSpan.MockContext]
+    ctx.spanId() should be(original.spanId())
+    ctx.traceId() should be(original.traceId())
   }
 
-  it should "generate span context data" in {
-    val test: MockSpan.MockContext = testSpan().context()
-    assume(test.spanId() > 0L, "Span id wasn't initialized")
-    assume(test.traceId() > 0L, "Trace id wasn't initialized")
-
-    val result: Map[String, String] = TextMapCarrier.inject(tracer, test)
-
-    result should not be 'isEmpty
-    result.size should be (2)
-    result.get("spanid").isEmpty should be (false)
-    result.get("traceid").isEmpty should be (false)
-  }
-
-  it should "generate baggage items" in {
+  it should "handle baggage" in {
     val test = testSpan().setBaggageItem("key1", "value1").setBaggageItem("key2", "value2").context()
     assume(test.baggageItems().iterator().hasNext, "Test baggage wasn't created")
 
-    val result: Map[String, String] = TextMapCarrier.inject(tracer, test)
+    val carrier: Map[String, String] = TextMapCarrier.inject(tracer, test)
+    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, carrier)
 
-    result.get("key1") should be (Some("value1"))
-    result.get("key2") should be (Some("value2"))
+    result shouldBe 'isSuccess
+    val it = result.get.baggageItems().iterator()
+    val first = it.next()
+    val second = it.next()
+    it.hasNext should be(false)
+
+    "key1".equals(first.getKey) | "key1".equals(second.getKey) should be(true)
+    "value2".equals(first.getValue) | "value2".equals(second.getValue) should be(true)
   }
 
-  it should "extract payload data" in {
-    val test: Map[String, String] = Map(("spanid", "13"),("traceid", "17"))
-
-    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, test)
-
-    result.isSuccess should be (true)
-    val mock = result.get.asInstanceOf[MockSpan.MockContext]
-    mock.spanId() shouldBe (13L)
-    mock.traceId() shouldBe (17L)
-  }
-
-  it should "handle a malformed payload" in {
-    val test: Map[String, String] = Map(("bad_key", "bad_value"))
-
-    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, test)
+  it should "handle an empty payload" in {
+    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, Map.empty)
 
     result shouldBe 'isFailure
   }
 
-  it should "extract baggage items" in {
-    val test: Map[String, String] = Map(("spanid", "13"),("traceid", "17"),("key1", "value1"))
+  it should "handle a malformed payload" in {
+    val malformed: Map[String, String] = Map(("bad_key", "bad_value"))
 
-    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, test)
+    val result: Try[SpanContext] = TextMapCarrier.extract(tracer, malformed)
 
-    result shouldBe 'isSuccess
-    val resultIter = result.get.baggageItems().iterator()
-    resultIter.hasNext should be (true)
-    val baggage = resultIter.next()
-    baggage.getKey shouldBe ("key1")
-    baggage.getValue shouldBe ("value1")
-    resultIter.hasNext should be (false)
+    result shouldBe 'isFailure
   }
-
 }
